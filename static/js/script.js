848970b4bedc5b082a53675f4ca6555b21f91cc1
CathalDolan <cathal@gizagig.com>
2020-11-03 23:28:41 +0000

    Tooltop functionality added. Applies to input icons on Register and Create pages.

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.tabs').tabs();
     $('select').formSelect();
+    $('.tooltipped').tooltip();
+    $('#textarea1').val('');
+    M.textareaAutoResize($('#textarea1'));
 });
+
